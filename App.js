7284fd911481b7df53362db56e309b822ec1dc51
BaoNguyen <toibaonguyen@gmail.com>
2022-05-24 21:00:34 +0700

    asfasf

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import {Text, StyleSheet, View} from 'react-native';
 import React, {Component} from 'react';
 import Setting from './Settings/Setting';
 import Extensionandeconomicservice from './Extension/Extensionandeconomicservice';
+import Bill from './Extension/Screens/Bill';
 
 export default class App extends Component {
   render() {
-    return <Extensionandeconomicservice/>;
+    return <Bill/>;
   }
 }
 
--- Extension/Extensionandeconomicservice.js
+++ Extension/Extensionandeconomicservice.js
@@ -5,18 +5,18 @@ import Ecustombutton from './Ecustombutton';
 
 export default function Extensionandeconomicservice(){
     return(
-        <View style={{flex:1,alignItems:"center"}}>
-            <View style={{flex:1,alignItems:"center",backgroundColor:"white",borderRadius:10}}>
-                <Text style={{marginTop:10,fontSize:40,marginBottom:20}}>Tiện ích</Text>
+        <View style={{alignItems:"center"}}>
+            <View style={{alignItems:"center",backgroundColor:"white",borderRadius:10}}>
+                <Text style={{marginTop:10,fontSize:30,marginBottom:20}}>Tiện ích</Text>
                 <Ecustombutton Text="Thanh toán hóa đơn" icon="browser" onPress={()=>{}}/>
                 <Ecustombutton Text="Nạp tiền điện thoại" icon="phone" onPress={()=>{}}/>
                 <Ecustombutton Text="Vé máy bay, vé tàu" icon="ticket" onPress={()=>{}}/>
             </View>
 
-            <View style={{flex:1,alignItems:"center",backgroundColor:"white",borderRadius:10}}>
-            <Text style={{fontSize:40,marginBottom:20}}>Dịch vụ tài chính</Text>
-                <Ecustombutton Text="Bảo hiểm" icon="wallet" onPress={()=>{}}/>
-                <Ecustombutton Text="Tiết kiệm online" icon="phone" onPress={()=>{}}/>
+            <View style={{alignItems:"center",backgroundColor:"white",borderRadius:10,marginTop:20}}>
+            <Text style={{fontSize:30,marginBottom:20}}>Dịch vụ tài chính</Text>
+            <Ecustombutton Text="Bảo hiểm" icon="wallet" onPress={()=>{}}/>
+            <Ecustombutton Text="Tiết kiệm online" icon="area-graph" onPress={()=>{}}/>
 
 
             </View>
